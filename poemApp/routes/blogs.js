0edcc76dc58d4efe97ca00c5e274be135a0382ae
rutti-bep <s.konnno.train233@gmail.com>
2018-09-04 03:56:42 +0900

    add twitter-card #20

--- poemApp/routes/blogs.js
+++ poemApp/routes/blogs.js
@@ -10,12 +10,12 @@ router.get("/:id", function(req, res,next) {
   if(req.params.id.match(/^\d+$/)){
     var blogId = Number(req.params.id);
     dbController.SelectBlogs(blogId,(blog)=>{
-      console.log(blog);
+      console.log("https://"+req.headers.host+req.originalUrl);
       htmlController.AllParse(blog[0].text,(blogText)=>{
         if(blog.length == 0){
           res.render('pages/notfound');
         }else{
-          res.render('pages/blogs',{title:blog[0].title,blogText:blogText});
+          res.render('pages/blogs',{title:blog[0].title,url:"https://"+req.headers.host+req.originalUrl,description:"rutti-bepのblog,"+blog[0].title,blogText:blogText});
         }
       })
     })
